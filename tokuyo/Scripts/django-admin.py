b5ea38e8f1253c051c96ad19dea6d02cbd265960
Mnemo <h164654156465@gmail.com>
2018-10-20 22:22:46 +0800

    Make changes to UI

--- tokuyo/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\h164654156465\google drive\quick access\coding\project\django projects\tokuyo\tokuyo\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
